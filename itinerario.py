366b7188d29ef005d9940b20d990c13d7d81fa05
dodevs <douglass.sousa@outlook.com.br>
2018-03-01 23:24:20 -0300

    melhorias na funcao base

--- itinerario.py
+++ itinerario.py
@@ -3,13 +3,25 @@ import json
 
 ceturbUrlBase = "https://sistemas.es.gov.br/webservices/ceturb/onibus/api/"
 
+def get_rota(itinerario, sentido):
+    # Pega somente a rota do sentido escolhido
+    itinerarioI = [waypoint['Desc_Via'].strip() for waypoint in itinerario if waypoint['Sentido'] == sentido]
+    itinerarioIOrigin = itinerarioI[0]  # Ponto de origem
+    itinerarioIDestination = itinerarioI[-1]  # Ponto de destino
+    itinerarioIWaypoints = [item for item in itinerarioI[1:-2]]  # Pontos de passagem
+
+    return [itinerarioIOrigin, itinerarioIWaypoints, itinerarioIDestination]
+
 # Funcao para obter itinerario
 def get_itineraio(linha):
     response = requests.get(ceturbUrlBase+'BuscaItinerarios/'+linha)
     #Parsea o resultado em um json
-    parsed_response = json.loads(response.text)
+    parsedResponse = json.loads(response.text)
 
-    return parsed_response
+    return {
+        'I': get_rota(parsedResponse, 'I'),
+        'V': get_rota(parsedResponse, 'V')
+    }
 
 def main():
     itinerario = get_itineraio("843")
