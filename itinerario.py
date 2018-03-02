1951ff3f9b0173b163046c021e519c519993881d
dodevs <douglass.sousa@outlook.com.br>
2018-03-01 23:48:57 -0300

    Google maps Directions API function

--- itinerario.py
+++ itinerario.py
@@ -14,18 +14,41 @@ def getRota(itinerario, sentido):
 
 # Funcao para obter itinerario
 def getItineraio(linha):
-    response = requests.get(ceturbUrlBase+'BuscaItinerarios/'+linha)
+    itinerarioResponse = requests.get(ceturbUrlBase+'BuscaItinerarios/'+linha)
     #Parsea o resultado em um json
-    parsedResponse = json.loads(response.text)
+    parsedResponse = json.loads(itinerarioResponse.text)
 
     return {
         'I': getRota(parsedResponse, 'I'),
         'V': getRota(parsedResponse, 'V')
     }
 
+def getDirecoes(rota, sentido):
+    mapsUrl = "https://maps.googleapis.com/maps/api/directions/json"
+    apiKey = open('google_api_key.json', 'rt')
+    apiKey = json.loads(apiKey.read())['api_key']
+    ''' PARAMETROS DISPONIVEIS
+    
+    mode: driving (por estradas); walking (caminhada); bicycling (de bike); transit (transporte publico)
+    waypoints: optimize:true (Algoritmo do cacheiro viajante)
+    region: us, uk, br (Por pais)
+    '''
+    mapsParametros = {
+        'origin': rota[sentido][0],
+        'destination': rota[sentido][2],
+        'waypoints': "|".join([waypoint for waypoint in rota[sentido][1]]),
+        'region': 'br',
+        'key': apiKey
+    }
+
+    mapsResponse = requests.get(mapsUrl, mapsParametros)
+
+    return mapsResponse.text
+
+
 def main():
-    itinerario = getItineraio("843")
-    print(itinerario)
+    itinerario = getItineraio("860")
+    print(getDirecoes(itinerario, 'I'))
 
 if __name__ == "__main__":
     main()
